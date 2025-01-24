64ca9ac247454077f86664285848ce2e54aea0da
Sofiia-Khrystyna <sonia030605@gmail.com>
2025-01-24 21:31:40 +0100

    WASD movement

--- GGJ/Assets/Scripts/CameraMovement.cs
+++ GGJ/Assets/Scripts/CameraMovement.cs
@@ -1,11 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour
 {
+    private Vector2 direction;
     public void OnMove(InputAction.CallbackContext context)
     {
-        Vector2 movementVector = context.ReadValue<Vector2>();
-        Debug.Log(movementVector);
+        direction = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        float rotatedX = direction.y * (Mathf.PI / 4) + direction.x * (Mathf.PI / 4);
+        float rotatedY = direction.y * (Mathf.PI / 4) - direction.x * (Mathf.PI / 4);
+        transform.position = new Vector3(
+            transform.position.x + rotatedX * Time.deltaTime, 
+            0, 
+            transform.position.z + rotatedY * Time.deltaTime
+        );
     }
 }
