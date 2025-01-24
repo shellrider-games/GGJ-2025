a46012f031cee5bd6951cc932d2e7968780ff9a9
Georg Becker <georg.becker@shellrider.com>
2025-01-24 22:58:51 +0100

    Implements placing objects on world grid

--- GGJ/Assets/Scripts/PlaceObject.cs
+++ GGJ/Assets/Scripts/PlaceObject.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlaceObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject objectToPlace;
+    [SerializeField] private Collider theWorldPlane;
+    private Vector2 mousePosition;
+    
+    public void OnMouseMovement(InputAction.CallbackContext context)
     {
-        
+        mousePosition = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPlaceObject(InputAction.CallbackContext context)
     {
-        
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            if(theWorldPlane.Raycast(ray, out RaycastHit hit,100f))
+            {
+                Vector3 hitPosition = hit.point;
+                Vector3 placePosition = new Vector3(Mathf.Round(hitPosition.x) + .5f, Mathf.Round(hitPosition.y), Mathf.Round(hitPosition.z) + .5f);
+                Instantiate(objectToPlace, placePosition, Quaternion.identity).SetActive(true);
+            }
+        }
     }
+    
 }
