07c37e5f30486a63cd3fe7cf81b2b14f6a68f858
Sofiia-Khrystyna <sonia030605@gmail.com>
2025-01-24 22:15:26 +0100

    mouse direction set up

--- GGJ/Assets/Scripts/CameraMovement.cs
+++ GGJ/Assets/Scripts/CameraMovement.cs
@@ -4,14 +4,18 @@ using UnityEngine.InputSystem;
 public class CameraMovement : MonoBehaviour
 {
     private const float Degrees45 = (Mathf.PI / 4);
-    private Vector2 direction;
+    private Vector2 keyboardDirection;
+    private Vector2 mouseDirection;
+    private bool useKeyboard = false;
+    
     public void OnMove(InputAction.CallbackContext context)
     {
-        direction = context.ReadValue<Vector2>();
+        keyboardDirection = context.ReadValue<Vector2>();
     }
 
     private void Update()
     {
+        Vector2 direction = useKeyboard ? keyboardDirection.normalized : mouseDirection.normalized;
         float rotatedX = direction.y * Degrees45 + direction.x * Degrees45;
         float rotatedY = direction.y * Degrees45 - direction.x * Degrees45;
         transform.position = new Vector3(
@@ -19,5 +23,39 @@ public class CameraMovement : MonoBehaviour
             0, 
             transform.position.z + rotatedY * Time.deltaTime
         );
+    }
+
+    public void OnMousePosition(InputAction.CallbackContext context)
+    {
+        Vector2 mousePosition = context.ReadValue<Vector2>();
+        
+        useKeyboard = true;
+        mouseDirection = Vector2.zero;
+        
+        if (mousePosition.x < 32)
+        {
+            useKeyboard = false;
+            mouseDirection.x = -1;
+        }
+
+        if (mousePosition.x > Screen.width - 32)
+        {
+            useKeyboard = false;
+            mouseDirection.x = 1;
+        }
+
+        if (mousePosition.y < 32)
+        {
+            useKeyboard = false;
+            mouseDirection.y = -1;
+        }
+        
+        if (mousePosition.y > Screen.height - 32)
+        {
+            useKeyboard = false;
+            mouseDirection.y = 1;
+        }
+        
+        Debug.Log(mousePosition);
     }
 }
