f043a32a6313bb58c961610fb7f60cbd5b88b015
Xaver <xdxaverdrabik>
2025-01-25 16:02:35 +0100

    "Buymode" implemented

--- GGJ/Assets/Scripts/Object/PlaceObject.cs
+++ GGJ/Assets/Scripts/Object/PlaceObject.cs
@@ -6,15 +6,17 @@ public class PlaceObject : MonoBehaviour
     [SerializeField] private GameObject objectToPlace;
     [SerializeField] private Collider theWorldPlane;
     private Vector2 mousePosition;
+    private Statemanager _statemanager;
     
     public void OnMouseMovement(InputAction.CallbackContext context)
     {
         mousePosition = context.ReadValue<Vector2>();
+        _statemanager = GetComponent<Statemanager>();
     }
 
     public void OnPlaceObject(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Performed)
+        if (context.phase == InputActionPhase.Performed && _statemanager.CurrentState == "place")
         {
             Ray ray = Camera.main.ScreenPointToRay(mousePosition);
             if(theWorldPlane.Raycast(ray, out RaycastHit hit,100f))
@@ -26,6 +28,8 @@ public class PlaceObject : MonoBehaviour
 
                 Instantiate(objectToPlace, placePosition, desiredRotation).SetActive(true);
             }
+            
+            _statemanager.Idle();
         }
     }
 }
