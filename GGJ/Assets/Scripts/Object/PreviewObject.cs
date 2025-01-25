9872ae9d2d958aad2ec9bb181646278a19c5b129
Xaver <xdxaverdrabik>
2025-01-25 12:31:39 +0100

    Previewing Gameobjects Before Placing

--- GGJ/Assets/Scripts/Object/PreviewObject.cs
+++ GGJ/Assets/Scripts/Object/PreviewObject.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,11 +6,21 @@ public class PreviewObject : MonoBehaviour
 {
     [SerializeField] private GameObject objectToPreview;
     [SerializeField] private Collider theWorldPlane;
-    
+
+    private GameObject _previewObject;
+    private void Start()
+    {
+        _previewObject = Instantiate(objectToPreview, Vector3.zero, Quaternion.identity);
+        _previewObject.SetActive(true);
+    }
+
     public void OnMouseMovement(InputAction.CallbackContext context)
     {
         Vector2 mousePosition = context.ReadValue<Vector2>();
-        
-        
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        if (theWorldPlane.Raycast(ray, out RaycastHit hit, 100f))
+        {
+            _previewObject.transform.position = HelperFunctions.WorldPosToGrid(hit.point);
+        }
     }
 }
