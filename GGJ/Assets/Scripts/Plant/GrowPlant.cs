b897db347dbc945ad73936a096eb4ae9a90c73c7
Sofiia-Khrystyna <sonia030605@gmail.com>
2025-01-26 16:00:14 +0100

    Kinda humidifier

--- GGJ/Assets/Scripts/Plant/GrowPlant.cs
+++ GGJ/Assets/Scripts/Plant/GrowPlant.cs
@@ -10,8 +10,10 @@ public class GrowPlant : MonoBehaviour
     [SerializeField] private List<AudioClip> plantSounds;
     [SerializeField] private GameObject startState;
     [SerializeField] private GameObject endState;
-    [Range(0f, 50f)] public float growthTime;
-    [Range(0f, 20f)] public float randomizeGrowthTime=3f;
+    [SerializeField] private GameObject humidifier;
+    [Range(0f, 50f)] public float baseGrowthTime = 5f;
+    [Range(0f, 20f)] public float randomizeGrowthTime = 10f;
+    [Range(0f, 10f)] public float proximityThreshold = 3f;
 
     private Coroutine growingCoroutine;
     
@@ -31,6 +33,11 @@ public class GrowPlant : MonoBehaviour
 
     private IEnumerator StartGrowPlant()
     {
+        float distanceToHumidifier = Vector3.Distance(transform.position, humidifier.transform.position);
+        Debug.Log("Planter pos:" + transform.position);
+        Debug.Log("Humidifier pos:" + humidifier.transform.position);
+        float growthTime = distanceToHumidifier <= proximityThreshold ? baseGrowthTime / 2 : baseGrowthTime;
+        Debug.Log("Growth time:" + baseGrowthTime);
         yield return new WaitForSeconds(growthTime+Random.Range(0, randomizeGrowthTime));
         
         AudioSource.PlayClipAtPoint(plantSounds[Random.Range(0, plantSounds.Count)], transform.position);
