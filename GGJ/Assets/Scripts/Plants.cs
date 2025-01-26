4acea1d52d56e4cfb7b76afb4dd751892bcc7034
Florian <cc231031@fhstp.ac.at>
2025-01-26 11:36:42 +0100

    Sounds imported & Plant sound Script

--- GGJ/Assets/Scripts/Plants.cs
+++ GGJ/Assets/Scripts/Plants.cs
@@ -1,28 +1,40 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Plants : MonoBehaviour
 {
+    [SerializeField] private List<AudioClip> plantSounds; // List of audio clips to choose from
     [SerializeField] private GameObject startState;
     [SerializeField] private GameObject endState;
     [Range(0, 20)] public float growthTime;
-    
-    private float timer = 0f;
 
-    public void Start()
+    private void Start()
     {
         startState.SetActive(true);
         endState.SetActive(false);
+
+        // Start the growth coroutine
+        StartCoroutine(GrowPlant());
     }
 
-    public void Update()
+    private IEnumerator GrowPlant()
     {
-        timer += Time.deltaTime;
+        float timer = 0f;
 
-        if (timer >= growthTime)
+        // Wait until the growth time is reached
+        while (timer < growthTime)
         {
-            startState.SetActive(false);
-            endState.SetActive(true);
+            timer += Time.deltaTime;
+            yield return null; // Wait for the next frame
         }
+
+        // Play a random sound from the list
+        if (plantSounds != null && plantSounds.Count > 0)
+            AudioSource.PlayClipAtPoint(plantSounds[UnityEngine.Random.Range(0, plantSounds.Count)], transform.position);
+
+        // Switch states
+        startState.SetActive(false);
+        endState.SetActive(true);
     }
 }
